a979f2ea46dd463a8f2d06e75c8f78ce621f5228
NoahBakayou <NoahBakayou@gmail.com>
2022-09-19 14:38:26 -0700

    Whatever the heck I wrote surprisingly works, I just need help with stopping the loop and redrawing the grid with the X in the spot they died
    Robert helped me again what a g

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,24 @@
 public class LinkedList {
+    Node headNode; //node type instance variable
+
+    public void addHeadNode(int x, int y){
+
+        Node newNode = new Node();
+
+        newNode.xPosition = x;
+        newNode.yPosition = y;
+
+        newNode.nextNode = headNode;
+
+        headNode = newNode;
+
+    }
+
+    public Node removeHeadNode(){
+        headNode = headNode.nextNode;
+        return headNode;
+    }
+
+
 
 }
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,5 @@
 public class Node {
+    public static int xPosition; //instance variable
+    public static int yPosition; //instance variable
+    Node nextNode = null; //node type instance variable
 }
